ae66b45030fe6e6e622c788945c0fc3ff0b68155
HoangAnh <anhht.fe@gmail.com>
2026-02-25 22:54:32 +0700

    section-2 folders and packages

--- src/app.js
+++ src/app.js
@@ -1,15 +1,16 @@
+const compression = require("compression");
 const express = require("express");
+const { default: helmet } = require("helmet");
 const { compile } = require;
 ("morgan");
 const morgan = require("morgan");
 const app = express();
 
 //init middlewares
-// app.use(morgan("dev"));
-app.use(morgan("combined"));
-// morgan("common");
-// morgan("short");
-// morgan("tiny");
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
 // init db
 
 //init routes
@@ -17,6 +18,17 @@ app.get("/", (req, res, next) => {
   res.status(200).json({
     message: "welcome to nodejs architecture",
   });
+});
+app.get("/data", (req, res) => {
+  const bigData = {
+    users: Array.from({ length: 1000 }, (_, i) => ({
+      id: i,
+      name: `User ${i}`,
+      email: `user${i}@example.com`,
+      address: `123 Street ${i}, City, Country`,
+    })),
+  };
+  res.status(200).json({ bigData });
 });
 // handling error
 
